36ec48eb59f37ff90b6656bd51e2c84d84092204
wscats <2587575267@qq.com>
2017-07-25 11:09:49 +0800

    muse mui

--- 19.mui/app/main.js
+++ 19.mui/app/main.js
@@ -9,48 +9,68 @@ import VueResource from 'vue-resource';
 import axios from "axios";
 //轮播图
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+//导入JS
+import MuseUI from 'muse-ui';
+//导入样式
+import 'muse-ui/dist/muse-ui.css';
+//import "mui";
+import "mui/dist/css/mui.css";
+import mui from "mui/dist/js/mui.js";
+window.mui = mui;
+Vue.use(MuseUI)
 Vue.use(Vuex);
 //通过 Vue.use()明确地安装路由功能
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(VueAwesomeSwiper)
-//挂载axios在Vue构造器下
+	//挂载axios在Vue构造器下
 Vue.prototype.$ajax = axios;
 //拥入weui样式库
 import "weui"
 //引入组件
 import home from "./components/routes/home.vue";
-import xecharts from "./components/xecharts.vue";
-import xmap from "./components/xmap.vue";
-import xvux from "./components/xvux.vue";
+//选项卡
+import nav1 from "./components/navs/nav1.vue";
+import nav2 from "./components/navs/nav2.vue";
+import nav3 from "./components/navs/nav3.vue";
+//列表
+import list1 from "./components/list/list1.vue";
+import list2 from "./components/list/list2.vue";
+
+import app from "./components/routes/app.vue";
+import muicp from "./components/routes/mui.vue";
 
 var router = new VueRouter({
 	routes: [{
-			path: '/index',
-			component:home
+		path: '/index',
+		component: home,
+		children: [{
+			path: 'tab1',
+			component: nav1,
+			children: [{
+				path: 'list1',
+				component: list1,
+			}, {
+				path: 'list2',
+				component: list2,
+			}]
 		}, {
-			path: '/detail',
-			component: {
-				template: `
-							<div>
-								详情页
-							</div>
-						`
-			}
+			path: 'tab2',
+			component: nav2
 		}, {
-			path: '/echarts',
-			component: xecharts
-		}, {
-			path: '/map',
-			component: xmap
-		}, {
-			path: '/vux',
-			component: xvux
-		}, {
-			path: '/',
-			redirect: '/index'
+			path: 'tab3',
+			component: nav3
 		}]
-		// （缩写）相当于 routes: routes
+	}, {
+		path: '/app',
+		component: app,
+	}, {
+		path: '/mui',
+		component: muicp,
+	}, {
+		path: '/',
+		redirect: '/index/tab1/list1'
+	}]
 });
 
 //新建一个状态管理
@@ -83,12 +103,12 @@ var store = new Vuex.Store({
 		},
 		setNews(state) {
 			axios.get('https://cnodejs.org/api/v1//topics')
-			.then((response) => {
-				state.news = response.data.data
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+				.then((response) => {
+					state.news = response.data.data
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 		}
 	},
 	//action就是触发mutations
